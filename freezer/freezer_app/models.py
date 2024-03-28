3bb0c74ace2e29b228aed4664141700bc9602364
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-03-28 12:22:18 +0100

    Create FoodItem model
    This model represents a specific item that can be registered in a freezer
    It is expected to fall under some FoodCategory

--- freezer/freezer_app/models.py
+++ freezer/freezer_app/models.py
@@ -13,3 +13,24 @@ class FoodCategory (models.Model):
     class Meta:
         verbose_name = 'Food category'
         verbose_name_plural = 'Food categories'
+
+
+class FoodItem(models.Model):
+    """
+    This model represents a specific item that can be registered in the freezer, e.g. 'beef stew'.
+    It is expected to fall under some FoodCategory, e.g. 'main course'.
+    The item holds an awareness of whether it is currently registered in the freezer at all or not.
+    It is also described by an amount (a non-negative integer).
+    If the item is not registered in the freezer at the moment, it automatically has a zero amount.
+    """
+    name = models.CharField(max_length=45, verbose_name="food item")
+    category = models.ForeignKey(FoodCategory, null=True, on_delete=models.SET_NULL)  # FoodItems are expected to have a category. However, deleting a category does not delete all the items, they stay without category
+    currently_registered = models.BooleanField(verbose_name="currently registered", default=True) # specifies whether the existing item is currently listed in the freezer
+    amount = models.IntegerField(verbose_name="food amount", default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category}) - amount: {self.amount}'
+
+    class Meta:
+        verbose_name = "Food item"
+        verbose_name_plural = "Food items"
