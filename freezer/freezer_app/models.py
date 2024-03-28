7321ddc739c4efe8f7a60beaae3bea3452e16340
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-03-28 12:03:03 +0100

    Create first model -> FoodCategory
    Simple class inheriting from django.db.models.Model
    Represents an abstract category like 'main dish', 'soup', 'semi-finished product'

--- freezer/freezer_app/models.py
+++ freezer/freezer_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class FoodCategory (models.Model):
+    """
+    This model represents the food category, e.g. 'main course', 'soup', 'semi-finished product' etc.
+    """
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Food category'
+        verbose_name_plural = 'Food categories'
