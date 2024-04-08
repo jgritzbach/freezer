8fa87bd6d032bb31f054446c046f2055270bafdc
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-04-08 16:05:10 +0200

    Create necessary imports in freezer_app views.py

--- freezer/freezer_app/views.py
+++ freezer/freezer_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.contrib import messages
+from .models import FoodItem, FoodCategory
 
 # Create your views here.
