4a703b8f2b998ea5fd642d0dd5a34901856d38bb
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-04-08 16:11:58 +0200

    Create 'Recordset' view class
    This view will be responsible for showing all food items currently registered in the freezer

--- freezer/freezer_app/views.py
+++ freezer/freezer_app/views.py
@@ -4,3 +4,8 @@ from django.contrib import messages
 from .models import FoodItem, FoodCategory
 
 # Create your views here.
+class Recordset(generic.ListView):
+    """
+    The view displays all items currently kept in the database as "currently_registered"
+    """
+    template_name = 'freezer_app/recordset.html'
